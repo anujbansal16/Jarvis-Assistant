50c4f03cc4332a4215eea1ba768fc80ff169198c
anujbansal16 <anuj.bansal16@gmail.com>
2020-07-26 17:15:32 +0530

    Added youtube search and play

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+# pip install SpeechRecognition
+# pip install PyAudio
 import speech_recognition as sr
+# pip install pyttsx3
 import pyttsx3
 from datetime import date
 import webbrowser
 import time
 from time import gmtime, strftime
 import wikipedia
+# pip install google
 from googlesearch import search 
+# pip install youtube-search
+from youtube_search import YoutubeSearch
+
+
+
 
 
 JARVISMSG="How May i help you?, Sir"
@@ -13,9 +22,13 @@ NOTRECOGNIZED="Sorry Sir, I cant understand it"
 EXITMESSAGE="Bye Bye, Sir"
 
 
+
+
 engine = pyttsx3.init()
 voices = engine.getProperty('voices') 	
 r = sr.Recognizer()
+# r.energy_threshold =4000
+# r.dynamic_energy_threshold = True
 
 
 ###########################################
@@ -24,22 +37,6 @@ r = sr.Recognizer()
 def speak(text):
 	engine.say(text)
 	engine.runAndWait()
-
-def takeCommand():
-	with sr.Microphone() as source:
-		r.adjust_for_ambient_noise(source)
-		r.pause_thresold=1
-		speak("Waiting for command , Sir")
-		# audio = r.adjust_for_ambient_noise(source)	
-		audio = r.listen(source)
-		try:
-			speak("Okay")
-			query=r.recognize_google(audio,language="en-in")
-			print(query)
-			return query
-		except Exception as e:
-			speak("Sorry, Sir I cant Recognize that, Sir")
-			return None
 
 def exitMsg():
 	speak(EXITMESSAGE)
@@ -55,8 +52,10 @@ def wikiData(query,sentences=1):
 		wikiData=wikipedia.summary(query,sentences=sentences,auto_suggest=True)	
 		print(wikiData)
 		speak(wikiData)
+		return 1
 	except Exception as e:
-		speak("Sorry Sir, I cant find anything related to that")
+		speak("Sorry Sir, I cant find anything related to that on wikipedia")
+		return None
 
 
 
@@ -78,32 +77,68 @@ def openArticles(query):
 	article_url=getGoogleResultLink(articles_it)	
 	openTab(article_url)
 
+def playMusic(query):
+	results = YoutubeSearch(query, max_results=1).to_dict()
+	speak("Playing: {}".format(results[0]["title"]))
+	url = "https://www.youtube.com/{}".format(results[0]["url_suffix"])
+	openTab(url)
+
+def processQuery(query):
+	if query:
+		query=query.lower()
+		if query.startswith("google"):
+			query=query.replace("google","",1)
+			url="https://www.google.com.tr/search?q={}".format(query)
+			openTab(url)
+
+		elif query.startswith("open"):
+			speak("opening, Sir")
+			query=query.replace("open","",1)
+			openArticles(query)
+		elif "article on" in query:
+			#open google links
+			speak("opening, Sir")
+			ln=(len("article on"))
+			query=query[query.find("article on")+ln+1:]
+			print(query)
+			openArticles(query)
+
+		elif query.startswith("youtube"):
+			query=query.replace("youtube","",1)
+			url="https://www.youtube.com/results?search_query=".format(query)
+			openTab(url)
+
+		elif query.startswith("play"):
+			query=query.replace("youtube","",1)
+			playMusic(query)
+		else:
+			if not wikiData(query):
+				speak("Let me try to search it somewhere else for you, Sir")
+				speak("Opening on browser ,Sir" )
+				openArticles(query)
 
 def startThread():
 	speak(JARVISMSG)
 	while True:
-		query=takeCommand();
-		if query:
-			query=query.lower()
-			if query.startswith("google"):
-				query=query.replace("google","",1)
-				url="https://www.google.com.tr/search?q={}".format(query)
-				openTab(url)
-			elif "article on" in query or query.startswith("open"):
-				#open google links
-				query=query.replace("article on","",1)
-				openArticles(query)
-
-			elif query.startswith("youtube"):
-				query.replace("youtube","",1)
-				url = "https://www.youtube.com/results?search_query={}".format(query)
-				openTab(url)
+		with sr.Microphone() as source:
+			r.pause_thresold=1
+			# audio = r.adjust_for_ambient_noise(source)	
+			speak("Waiting for command , Sir")
+			audio = r.listen(source)
+			try:
+				speak("Okay")
+				query=r.recognize_google(audio,language="en-in")
+				print(query)
+			except Exception as e:
+				speak("Sorry, Sir I cant Recognize that, Sir")
+				query=None
 			
-			elif "bye" in query or query=="exit":
+			if (query and ("bye" in query or query=="exit")):
 				exitMsg()
 				return
-			else:
-				wikiData(query)	
+			processQuery(query)
+
+			
 		
 
 if __name__ == '__main__':
